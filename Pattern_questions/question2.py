56872340bdf9f7eb1f651e87567db98e062dff99
prashantnegi10 <54592987+prashantnegi10@users.noreply.github.com>
2022-01-06 11:47:33 +0530

    Add files via upload

---
+++ Pattern_questions/question2.py
@@ -0,0 +1,13 @@
+"""
+output pattern
+* 
+* * 
+* * * 
+* * * * 
+"""
+n=int(input('enter a no: '))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print("\n")
+        
